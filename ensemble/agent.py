6b5e9ff284093b9c61269a830c8a1b034215260a
seancraven <seanpietercraven@gmail.com>
2023-06-09 14:07:19 +0100

    First pass of writing the agent for jax

--- ensemble/agent.py
+++ ensemble/agent.py
@@ -1,10 +1,13 @@
+from typing import Tuple
+
 import gymnasium as gym
 import haiku as hk
 import jax
 import jax.numpy as jnp
 import numpy as np
-from jax.numpy import ndarray  # pyright: ignore
-from jax.random import PRNGKey
+from jax import Array
+from jax.nn import log_softmax
+from jax.random import KeyArray
 from jax.typing import ArrayLike
 
 
@@ -51,5 +54,42 @@ class Agent(hk.Module):
                 hk.Linear(action_space.n),
             ]
         )
-    def get_action(self,key: PRNGKey,  state: ArrayLike,) -> ndarray:
-        self.
+        self.transformed_actor = self.get_actor_params()
+        self.actor_params = self.transformed_actor.init(jnp.zeros((1, self.input_dim)))
+        self.transformed_critic = self.get_critic_params()
+        self.critic_params = self.transformed_critic.init(
+            jnp.zeros((1, self.input_dim))
+        )
+
+    def actor_forward(self, state: ArrayLike) -> Array:
+        return self.transformed_actor.apply(self.actor_params, state)
+
+    def critic_forward(self, state: ArrayLike) -> Array:
+        return self.transformed_critic.apply(self.critic_params, state)
+
+    def get_actor_params(self) -> hk.Transformed:
+        return hk.without_apply_rng(hk.transform(self.actor))
+
+    def get_critic_params(self) -> hk.Transformed:
+        return hk.without_apply_rng(hk.transform(self.critic))
+
+    def get_action(
+        self, key: KeyArray, state: ArrayLike
+    ) -> Tuple[Array, Array, Array, Array, KeyArray]:
+        """Selects an action from the agent's policy.
+
+        Args:
+            key (KeyArray): A PRNG key.
+            state (ArrayLike): The current state of the environment.
+
+        Returns:
+            Tuple[Array, Array, Array, Array, KeyArray]: A tuple containing the action, log probability of the action, the state value, the entropy of the policy and the new PRNG key.
+        """
+
+        logits = self.actor_forward(state)
+        key, subkey = jax.random.split(key)
+        actions = jax.random.categorical(subkey, logits)
+        log_prob = log_softmax(logits)
+        entropy = -jnp.sum(log_prob * jnp.exp(log_prob), axis=-1)
+        state_values = self.critic_forward(state)
+        return actions, log_probs, state_values, entropy, subkey
